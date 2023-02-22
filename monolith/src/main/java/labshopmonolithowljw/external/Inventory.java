b064ed04018d992706ed6e7992742e6c50856221
Lee Jangwon <owljwlee@users.noreply.github.com>
2023-02-22 15:32:33 +0900

    commit

---
+++ monolith/src/main/java/labshopmonolithowljw/external/Inventory.java
@@ -0,0 +1,12 @@
+package labshopmonolithowljw.external;
+
+import lombok.Data;
+import java.util.Date;
+@Data
+public class Inventory {
+
+    private Long id;
+    private Long stock;
+}
+
+
---
+++ monolith/src/test/java/labshopmonolithowljw/common/CucumberSpingConfiguration.java
@@ -0,0 +1,12 @@
+package labshopmonolithowljw.common;
+
+
+import labshopmonolithowljw.MonolithApplication;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = { MonolithApplication.class })
+public class CucumberSpingConfiguration {
+    
+}
